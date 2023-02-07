9572057490d0c1c69ea40449d4146b9ac783c462
Daniel Melo <daniel.melo42@outlook.com>
2023-02-07 13:33:16 -0700

    fixed endpoint request

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -4,17 +4,32 @@ import 'package:crud_bloc/models/user.dart';
 import 'package:http/http.dart' as http;
 
 class UserRepository {
-  static const String baseUrl =
-      "https://api.mlab.com/api/1/databases/crud/collections";
+  static const String baseUrl = "data.mongodb-api.com";
 
   static const apiKey =
       "Xt64xDtWXTAgvBiQhLiCgiMhnktLagYnxg4J6jINasEiHk9T1lpSkIetECSWLJUq";
 
+  Map<String, dynamic> defaultBody = {
+    "dataSource": "Cluster0",
+    "database": "crud",
+    "collection": "users"
+  };
+
   Future<List<User>> getAllUsers() async {
-    var response = await http.get(Uri(path: "$baseUrl/users?$apiKey"));
-    Iterable userList = json.decode(response.body);
-    List<User> users = userList.map((user) => User.fromJson(user)).toList();
-    return users;
+    try {
+      var response = await http.post(
+          Uri(
+              scheme: "https",
+              host: baseUrl,
+              path: "app/data-bjiti/endpoint/data/v1/action/find"),
+          body: json.encode(defaultBody),
+          headers: {"Content-Type": "application/json", "api-key": apiKey});
+      Iterable userList = json.decode(response.body)["documentss"];
+      List<User> users = userList.map((user) => User.fromJson(user)).toList();
+      return users;
+    } catch (e) {
+      throw Exception("Error trying to get users");
+    }
   }
 
   Future<User> getUserById(String id) async {
