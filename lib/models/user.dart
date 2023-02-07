8e88596ac89b2f4ed768a800064f25e33c17f1d1
Daniel Melo <daniel.melo42@outlook.com>
2023-02-07 08:59:40 -0700

    user model created

---
+++ lib/models/user.dart
@@ -0,0 +1,27 @@
+class User {
+  final String id;
+  final String firstName;
+  final String surname;
+  final String email;
+
+  User(
+      {required this.id,
+      required this.firstName,
+      required this.surname,
+      required this.email});
+
+  User.fromJson(Map json)
+      : id = json["_id"]["\$oid"],
+        firstName = json["first_name"],
+        surname = json["surname"],
+        email = json["email"];
+
+  Map toJson() {
+    return {
+      'id': id,
+      'first_name': firstName,
+      'surname': surname,
+      'email': email
+    };
+  }
+}
